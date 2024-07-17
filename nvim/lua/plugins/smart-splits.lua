e1aed7c77ec8e0127f90fb1c73807f742189704f
Brian Schiller <bgschiller@gmail.com>
2024-07-17 09:12:43 -0700

    kitty dir

---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,41 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  build = "./kitty/install-kittens.bash",
+  keys = {
+    {
+      "<C-h>",
+      function()
+        require("smart-splits").move_cursor_left()
+      end,
+      desc = "Move to left window",
+    },
+    {
+      "<C-j>",
+      function()
+        require("smart-splits").move_cursor_down()
+      end,
+      desc = "Move to below window",
+    },
+    {
+      "<C-k>",
+      function()
+        require("smart-splits").move_cursor_up()
+      end,
+      desc = "Move to above window",
+    },
+    {
+      "<C-l>",
+      function()
+        require("smart-splits").move_cursor_right()
+      end,
+      desc = "Move to right window",
+    },
+    {
+      "<C-\\>",
+      function()
+        require("smart-splits").move_cursor_previous()
+      end,
+      desc = "Move cursor to previous window",
+    },
+  },
+}
