3fe0a90a53a8aa6acdf791a33165a43fc8b67112
Brian Schiller <bgschiller@gmail.com>
2025-07-25 11:47:48 -0700

    windows updates

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
 Set-PSReadLineOption -EditMode Emacs
 $global:DefaultUser = [System.Environment]::UserName
 oh-my-posh init pwsh --config ~\dotfiles\oh-my-posh.omp.toml | Invoke-Expression
+Invoke-Expression (& { (jj util completion power-shell | Out-String) })
+
